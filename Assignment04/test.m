229411f54d95178a34dc2a287f282bb9b90d833e
Darren Jia <35616267+JavisDaDa@users.noreply.github.com>
2020-02-19 20:42:49 -0600

    HW4_1

--- Assignment04/test.m
+++ Assignment04/test.m
@@ -1,11 +1,4 @@
 I = imread('chessboard.jpg');
-
-figure; imshow(I);
-
-forward_dx = mipforwarddiff(I,'dx'); figure, imshow(forward_dx);
-
-forward_dy = mipforwarddiff(I,'dy'); figure, imshow(forward_dy);
-
-central_dx = mipcentraldiff(I,'dx'); figure, imshow(central_dx);
-
-central_dy = mipcentraldiff(I,'dy'); figure, imshow(central_dy);
+rotateI = imrotate(I, 30, 'nearest');
+imshow(I);
+imshow(rotateI);
